abbfa7509fca95d12180e19c2198886b157291cd
RajanishCoding <rajanishcoding500@gmail.com>
2025-08-16 21:28:37 +0530

    New Commit

--- Task2_QuoteApp/app/src/main/java/com/example/quoteapp/MainActivity.java
+++ Task2_QuoteApp/app/src/main/java/com/example/quoteapp/MainActivity.java
@@ -8,10 +8,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private ViewPager2 viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
 
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
 
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Quote");
+                    break;
+                case 1:
+                    tab.setIcon(R.drawable.round_favorite);
+                    break;
+            }
+        }).attach();
     }
 }
--- Task2_QuoteApp/app/src/main/java/com/example/quoteapp/QuoteFragment.java
+++ Task2_QuoteApp/app/src/main/java/com/example/quoteapp/QuoteFragment.java
@@ -1,64 +1,92 @@
 package com.example.quoteapp;
 
+import android.animation.TimeInterpolator;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.BounceInterpolator;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QuoteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QuoteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CardView quoteView;
+    private TextView quoteText;
+    private TextView authorText;
+    private LinearLayout buttonsLayout;
+    private Button favorB;
+    private Button shareB;
 
     public QuoteFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuoteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuoteFragment newInstance(String param1, String param2) {
-        QuoteFragment fragment = new QuoteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_quote, container, false);
+
+        quoteView = view.findViewById(R.id.quoteView);
+        quoteText = view.findViewById(R.id.quoteText);
+        authorText = view.findViewById(R.id.authorText);
+        buttonsLayout = view.findViewById(R.id.buttonsLayout);
+        favorB = view.findViewById(R.id.favorB);
+        shareB = view.findViewById(R.id.shareB);
+
+        quoteView.setAlpha(0f);
+        quoteView.setScaleY(0f);
+        quoteText.setAlpha(0f);
+        quoteText.setScaleY(0f);
+        authorText.setAlpha(0f);
+        authorText.setScaleX(0f);
+        buttonsLayout.setAlpha(0f);
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        startAnimation();
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quote, container, false);
-    }
+    private void startAnimation() {
+        quoteView.animate()
+                .scaleY(1f)
+                .alpha(1f)
+                .setDuration(800)
+//                .setInterpolator(new BounceInterpolator())
+                .start();
+
+        quoteText.setPivotY(0f);
+        quoteText.animate()
+                .withStartAction(() -> quoteText.setAlpha(1f))
+                .scaleY(1f)
+                .setStartDelay(800)
+                .setDuration(600)
+                .start();
+
+        authorText.animate()
+                .withStartAction(() -> quoteText.setAlpha(1f))
+                .scaleX(1f)
+                .alpha(1f)
+                .setStartDelay(1200)
+                .setDuration(600)
+                .start();
+
+        buttonsLayout.animate()
+                .alpha(1f)
+                .setStartDelay(1600)
+                .setDuration(600)
+                .start();
+   }
 }
